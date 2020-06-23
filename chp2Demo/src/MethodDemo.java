3533f4df64c28714450da993ab5fe3a0ce378186
WenHuiTan <67314492+WenHuiTan@users.noreply.github.com>
2020-06-23 17:00:08 +0800

    Update MethodDemo.java

--- chp2Demo/src/MethodDemo.java
+++ chp2Demo/src/MethodDemo.java
@@ -1,4 +1,14 @@
 
 public class MethodDemo {
-    
+    public static int sum(int x, int y){
+        return x+y;
+}
+    public static double sum(double x, double y){
+        return x+y;
+}
+    public static void main(String[] args){
+        int a =1;
+        int b =2;
+        double result = sum(a,b);
+    }
 }
